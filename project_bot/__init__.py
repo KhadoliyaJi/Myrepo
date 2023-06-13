61b9727d12b8b10515d925084a0e1f537e401a74
KhadoliyaJi <134993991+KhadoliyaJi@users.noreply.github.com>
2023-06-13 11:38:26 +0530

    Update __init__.py

--- project_bot/__init__.py
+++ project_bot/__init__.py
@@ -4,7 +4,7 @@ import logging
 
 import time
 
-openai.my_api_key = openai_key
+
 api_id = "27415472"
 api_hash = "33a013ead607b21bd2b77970b1b181e5"
 bot_token = "5871165882:AAFWx0ESawBJih8G4RQ6IgW_v4d5Cw2Arv4"
